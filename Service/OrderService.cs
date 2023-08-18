a2d186de90eb98f6eab359dfe402c1f68f76fb6c
Gustavo Andrade <gandrade@cardlytics.com>
2023-08-18 13:24:13 -0300

    Create service layer

---
+++ Service/OrderService.cs
@@ -0,0 +1,44 @@
+﻿using Domain.DTOs;
+using Domain.Interfaces;
+
+namespace Service;
+
+public class OrderService : IOrderService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public OrderService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public OrderDto CreateOrder(OrderPostDto order)
+    {
+        _unitOfWork.Orders.CreateOrder(order);
+        return null;
+    }
+
+    public OrderDto? FindById(int id)
+    {
+        var order = _unitOfWork.Orders.GetById(id);
+        if(order is null)
+            return null;
+
+        var orderDto = new OrderDto
+        {
+            Id = order.Id,
+            OrderDate = order.OrderDate,
+            TotalCost = order.OrderDetails.Sum(x => x.UnitPrice * x.Quantity),
+            OrderItems = order.OrderDetails.Select(oi => new OrderItemDto
+            {
+                Id = oi.Id,
+                ProductId = oi.ProductId,
+                ProductName = oi.Product.Name,
+                Quantity = oi.Quantity,
+                UnitPrice = oi.UnitPrice
+            }).ToList()
+        };
+
+        return orderDto;
+    }
+}
---
+++ Service/ProductService.cs
@@ -0,0 +1,31 @@
+﻿using Domain.DTOs;
+using Domain.Interfaces;
+
+namespace Service;
+
+public class ProductService : IProductService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ProductService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public ProductDto? FindById(int id)
+    {
+        var product = _unitOfWork.Products.GetById(id);
+
+        if (product is null)
+            return null;
+        
+        var productDto = new ProductDto
+        {
+            Id = product.Id,
+            ProductName = product.Name,
+            Price = product.Price
+        };  
+
+        return productDto;
+    }
+}
