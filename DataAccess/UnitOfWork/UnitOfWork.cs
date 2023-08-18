412f5f4d58a404df1d88cde0c77f0234e969aeec
Gustavo Andrade <gandrade@cardlytics.com>
2023-08-18 13:22:07 -0300

    Set up repository and unit of work pattern

---
+++ DataAccess/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.EF.Repositories;
+using Domain.Interfaces;
+
+namespace DataAccess.EF.UnitOfWork;
+
+public class UnitOfWork: IUnitOfWork
+{
+    private readonly ApplicationContext _context;
+    public IOrderRepository Orders { get; private set; }
+    public IProductRepository Products { get; private set; }
+
+    public UnitOfWork(ApplicationContext context)
+    {
+        _context = context;
+        Orders = new OrderRepository(_context);
+        Products = new ProductRepository(_context);
+    }
+
+    public int Complete() => _context.SaveChanges();
+    public void Dispose() => _context.Dispose();
+}
